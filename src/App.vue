e26d403dddc5299bed8bf21e35c68353d823dcfc
Peter Nocken <pn@r3think.io>
2021-12-12 17:08:28 +0100

    stuck on eslint problems

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
-  <HelloWorld />
+  <div>
+    <h3>Trending Information Monitoring System </h3>
+    <HelloWorld :width="1600" :height="500" />
+    <Datepicker v-model="date"></Datepicker>
+  </div>
+
 </template>
+
+
+
 
 <script>
 import { defineComponent } from 'vue'
@@ -14,13 +22,36 @@ export default defineComponent({
 })
 </script>
 
+<script>
+import Datepicker from 'vue3-date-time-picker';
+import 'vue3-date-time-picker/dist/main.css'
+
+export default {
+  components: { Datepicker },
+  data() {
+    return {
+      date: null,
+    };
+  }
+}
+</script>
+
+
+
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Arial", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  /*color: #2c3e50;*/
+  color: red;
   margin-top: 60px;
+
+}
+h3 {
+  color: turquoise;
+  font-size: 2rem;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,32 +3,57 @@ import { defineComponent } from 'vue'
 import { Line } from 'vue3-chart-v2'
 
 export default defineComponent({
-  name: 'SportChart',
+  name: 'TIMS',
   extends: Line,
   data () {
     return {
       state: {
         chartData: {
-          labels: ['Running', 'Swimming', 'Eating', 'Cycling'],
+          labels: ['1', '2', '3', '4', '5', '6', '7', '8','9', '10', '11', '12','13', '14', '15', '16'],
           datasets: [
             {
-              label: 'Human 1',
-              data: [20, 10, 4, 2]
+              label: 'Temperature',
+              data: [37, 35, 37, 35, 37, 35, 37, 35, 37, 35, 37, 35, 37, 35, 37, 35, 37, 35, 37, 35, 37, 35, 37, 35],
+              fill: false,
+              backgroundColor: "transparent",
+              borderColor: "red",
+              borderWidth: 1
             },
             {
-              label: 'Human 2',
-              data: [10, 20, 2, 4]
+              label: 'Humidity',
+              data: [100, 25, 100, 25, 100, 25, 100, 25, 100, 25, 100, 25, 100, 25, 100, 25, 100, 25, 100, 25, 100, 25],
+              backgroundColor: "transparent",
+              borderColor: "#36495d",
+              fill: false,
+              borderWidth: 1
             }
           ]
         },
         chartOptions: {
-          responsive: false
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              },
+              gridLines: {
+                display: true
+              }
+            }],
+            xAxes: [ {
+              gridLines: {
+                display: false
+              }
+            }]
+          },
+
+          responsive: true,
+          maintainAspectRatio: false
         }
       }
-    }
-  },
+     }
+ },
   mounted () {
     this.renderChart(this.state.chartData, this.state.chartOptions)
   }
-})
+   })
 </script>
